9751cfed3ee79d80986871c17f0a5bf86c81c291
Brooks Eshe <beshe@mail.greenriver.edu>
2019-01-16 22:16:22 -0500

    Added fat-free

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: brook
- * Date: 1/16/2019
- * Time: 10:12 AM
- */
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require
+require_once ('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
