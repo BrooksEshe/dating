cf5212f8525ce969d4e8d2c9366b193a44e7b2a6
Brooks Eshe <beshe@mail.greenriver.edu>
2019-02-11 16:08:32 -0500

    updated composer for classes

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+//require
+require_once ('vendor/autoload.php');
 session_start();
+
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
-//require
-require_once ('vendor/autoload.php');
 
 //create an instance of the Base class
 $f3 = Base::instance();
@@ -12,57 +13,9 @@ $f3 = Base::instance();
 //create states array
 $f3->set('states', array(
     'select'=>'Select',
-    'AL'=>"Alabama",
-    'AK'=>"Alaska",
-    'AZ'=>"Arizona",
-    'AR'=>"Arkansas",
     'CA'=>"California",
-    'CO'=>"Colorado",
-    'CT'=>"Connecticut",
-    'DE'=>"Delaware",
-    'DC'=>"District Of Columbia",
-    'FL'=>"Florida",
-    'GA'=>"Georgia",
-    'HI'=>"Hawaii",
-    'ID'=>"Idaho",
-    'IL'=>"Illinois",
-    'IN'=>"Indiana",
-    'IA'=>"Iowa",
-    'KS'=>"Kansas",
-    'KY'=>"Kentucky",
-    'LA'=>"Louisiana",
-    'ME'=>"Maine",
-    'MD'=>"Maryland",
-    'MA'=>"Massachusetts",
-    'MI'=>"Michigan",
-    'MN'=>"Minnesota",
-    'MS'=>"Mississippi",
-    'MO'=>"Missouri",
-    'MT'=>"Montana",
-    'NE'=>"Nebraska",
-    'NV'=>"Nevada",
-    'NH'=>"New Hampshire",
-    'NJ'=>"New Jersey",
-    'NM'=>"New Mexico",
-    'NY'=>"New York",
-    'NC'=>"North Carolina",
-    'ND'=>"North Dakota",
-    'OH'=>"Ohio",
-    'OK'=>"Oklahoma",
     'OR'=>"Oregon",
-    'PA'=>"Pennsylvania",
-    'RI'=>"Rhode Island",
-    'SC'=>"South Carolina",
-    'SD'=>"South Dakota",
-    'TN'=>"Tennessee",
-    'TX'=>"Texas",
-    'UT'=>"Utah",
-    'VT'=>"Vermont",
-    'VA'=>"Virginia",
     'WA'=>"Washington",
-    'WV'=>"West Virginia",
-    'WI'=>"Wisconsin",
-    'WY'=>"Wyoming"
 ));
 
 $f3->set('interests',array('TV','Puzzles','Movies','Reading','Cooking','Cards',
@@ -237,8 +190,6 @@ $f3->route('GET|POST /summary', function(){
     $template = new Template();
     echo $template->render('views/summary.html');
 });
-
-
 
 
 //run fat free
